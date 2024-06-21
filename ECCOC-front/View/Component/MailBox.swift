6bd85f26aec04b20e1143d79f2b264de5926afa9
koudaihirata <2230051@ecc.ac.jp>
2024-06-21 10:01:03 +0900

    メールページの画面完成（ルーティングはまだ分からないので実装してない）

---
+++ ECCOC-front/View/Component/MailBox.swift
@@ -0,0 +1,18 @@
+//
+//  MailBox.swift
+//  ECCOC-front
+//
+//  Created by Kodai Hirata on 2024/06/20.
+//
+
+import SwiftUI
+
+struct MailBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MailBox()
+}
