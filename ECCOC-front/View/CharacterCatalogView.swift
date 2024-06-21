ecb588b0aaf687e5370663e64993d689ec2396e6
koudaihirata <2230051@ecc.ac.jp>
2024-06-21 18:29:18 +0900

    GachaView完成した

---
+++ ECCOC-front/View/CharacterCatalogView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterCatalogView.swift
+//  ECCOC-front
+//
+//  Created by Kodai Hirata on 2024/06/21.
+//
+
+import SwiftUI
+
+struct CharacterCatalogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterCatalogView()
+}
